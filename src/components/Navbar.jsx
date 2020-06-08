1bc52d36399531f16fb849b90dcc4ebb546d9ba8
Dudja <dudja88@mail.ru>
2020-06-09 00:45:02 +0300

    added css.modules

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import s from './Navbar.module.css';
 
 const Navbar = ()=>{
   return (
-    <nav className='nav'>
-      menu
+    <nav className={s.nav}>
+      <ul>
+        <li>Друзья</li>
+        <li>Сообщения</li>
+        <li>Группы</li>
+      </ul>
     </nav>
   );
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import s from './Profile.module.css'
 
 const Profile = ()=>{
   return (
-    <div className='content'>
-      content
+    <div className={s.content}>
+      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRZFET3IBGGG6MRW_ykFqpgYyAo2dJ65SvtxUQJjc-8pmZPYpeU&usqp=CAU'/>
+      <div>Walter White 53y.o.</div>
     </div>
   );
 }
