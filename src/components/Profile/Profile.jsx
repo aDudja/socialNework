bd52e0c82bfb1cd1548742ae36db3e315aba1554
Dudja <dudja88@mail.ru>
2020-06-09 18:18:50 +0300

    new modules

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import s from './Profile.module.css'
+import MyPosts from './MyPosts/MyPosts';
 
 const Profile = ()=>{
   return (
     <div className={s.content}>
       <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRZFET3IBGGG6MRW_ykFqpgYyAo2dJ65SvtxUQJjc-8pmZPYpeU&usqp=CAU'/>
       <div>Walter White 53y.o.</div>
+      <textarea /><br/>
+      <button>Добавить новость</button>
+      <MyPosts />
     </div>
   );
 }
